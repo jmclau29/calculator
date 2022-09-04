79089c7b44e0b3098b896b69a6f53fbe799d6df2
Justin <jmclau29@gmail.com>
2022-09-04 23:57:11 +0900

    adds functions to change display, display divs, class & id changes

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+//mathematical functions
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -14,6 +15,7 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
+//operation function
 function operate(operator, num1, num2) {
     if (operator === '+') {
         return add(num1, num2);
@@ -28,3 +30,46 @@ function operate(operator, num1, num2) {
         return divide(num1, num2);
     }
 }
+
+function modifyFirst(newText) {
+
+    const firstNumber = document.getElementById("firstNumber");
+
+    if (firstNumber.textContent.length === 12) {
+        return;
+    }
+    if (firstNumber.textContent != '') {
+        firstNumber.textContent += newText;
+    } else {
+        firstNumber.textContent = newText;
+    }
+}
+
+function modifySecond(newText) {
+    const secondNumber = document.getElementById("secondNumber");
+
+    if (secondNumber.textContent.length === 12) {
+        return;
+    }
+    if (secondNumber.textContent != '') {
+        secondNumber.textContent += newText;
+    } else {
+        secondNumber.textContent = newText;
+    }
+}
+
+function addOperator(operatorText) {
+    const operatorDiv = document.getElementById("operator");
+
+    operatorDiv.textContent = operatorText;
+}
+
+const numberButton = document.querySelectorAll('.numberButton');
+numberButton.forEach(button => {
+    button.addEventListener('click', () => { modifyFirst(button.textContent); }, false);
+});
+
+const operatorButton = document.querySelectorAll('.operatorButton');
+operatorButton.forEach(button => {
+    button.addEventListener('click', () => { addOperator(button.textContent); }, false);
+});
