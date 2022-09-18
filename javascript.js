9810cbebe8e5545c7c3fdbaf078efd016d14687e
jmclau29 <90084211+jmclau29@users.noreply.github.com>
2022-09-18 19:15:36 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+let equation = {};
+
 //mathematical functions
 function add(num1, num2) {
     return num1 + num2;
@@ -23,26 +25,39 @@ function operate(operator, num1, num2) {
     if (operator === '-') {
         return subtract(num1, num2);
     }
-    if (operator === '*') {
+    if (operator === 'x') {
         return multiply(num1, num2);
     }
-    if (operator === '/') {
+    if (operator === '÷') {
         return divide(num1, num2);
     }
+}
+
+function newEquation(answer) {
+    equation.firstNum = answer;
+    modifyFirst(answer);
+    equation.secondNum = parseInt('');
+    modifySecond('');
+    equation.operator = '';
+    addOperator('');
 }
 
 function modifyFirst(newText) {
 
     const firstNumber = document.getElementById("firstNumber");
+    const answerDiv = document.getElementById('answer');
 
     if (firstNumber.textContent.length === 12) {
         return;
     }
-    if (firstNumber.textContent != '') {
+    if (firstNumber.textContent != '' && !answerDiv) {
         firstNumber.textContent += newText;
+        equation.firstNum += parseInt(newText);
     } else {
         firstNumber.textContent = newText;
+        equation.firstNum = parseInt(newText);
     }
+    
 }
 
 function modifySecond(newText) {
@@ -53,23 +68,51 @@ function modifySecond(newText) {
     }
     if (secondNumber.textContent != '') {
         secondNumber.textContent += newText;
+        equation.secondNum += parseInt(newText);
     } else {
         secondNumber.textContent = newText;
+        equation.secondNum = parseInt(newText);
     }
 }
 
 function addOperator(operatorText) {
     const operatorDiv = document.getElementById("operator");
+    const firstNumber = document.getElementById("firstNumber");
 
-    operatorDiv.textContent = operatorText;
+    if (firstNumber.textContent === '') {
+        return;
+    } else {
+        operatorDiv.textContent = operatorText;
+        equation.operator = operatorText;
+    }
 }
+
+
+
 
 const numberButton = document.querySelectorAll('.numberButton');
 numberButton.forEach(button => {
-    button.addEventListener('click', () => { modifyFirst(button.textContent); }, false);
+    button.addEventListener('click', () => {
+        if (operator.textContent === '') {
+            modifyFirst(button.textContent);
+        } else {
+            modifySecond(button.textContent);
+        };
+    }, false);
 });
 
 const operatorButton = document.querySelectorAll('.operatorButton');
 operatorButton.forEach(button => {
     button.addEventListener('click', () => { addOperator(button.textContent); }, false);
+});
+
+const equalButton = document.querySelector('#equals');
+equalButton.addEventListener('click', () => {
+    let answer = operate(equation.operator, equation.firstNum, equation.secondNum);
+
+    const answerDiv = document.getElementById('answer');
+    answerDiv.textContent = answer.toString();
+
+    newEquation(answer);
+    console.log(answer);
 });
